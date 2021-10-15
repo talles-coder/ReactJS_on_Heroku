c839df5575724a0e038392d4350c9a3e16e0f752
talles-coder <talleswendrel@gmail.com>
2021-10-15 14:20:47 -0300

    the first component was created

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Primeiro(params) {
+    return (
+            <div> 
+                <h1>Primeiro Componenta</h1>
+                <h2>Exemplo de um componente React</h2>
+            </div>
+    )
+}
+
+export default Primeiro
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './components/Primeiro'
+
+ReactDOM.render(
+    <Primeiro></Primeiro>,
+    document.getElementById('root')
+)
